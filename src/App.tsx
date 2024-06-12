b51a575ea1bcc0848aadde8cba17f0234df49bb9
JohNNy <johnnymundi@hotmail.com>
2024-06-11 21:48:01 -0300

    add search input logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ import { Box, Flex, Loader } from "monday-ui-react-core";
 const monday = mondaySdk();
 
 function App() {
-  //const [count, setCount] = useState(0)
-  const [boardData, setBoardData] = useState(null);
-  //const [error, setError] = useState(null);
+  const [boardData, setBoardData] = useState<any>(null); // Explicit any type
+  const [searchTerm, setSearchTerm] = useState<string>(""); // Estado para armazenar o termo de pesquisa
+  const [resultData, setResultData] = useState<any[]>([]); // Explicit any array type
+  const [columns, setColumns] = useState<any[]>([]);
 
   const main = `query {
     boards(ids: 6756792083) {
@@ -34,55 +35,113 @@ function App() {
   }`;
 
   useEffect(() => {
-    // Função para buscar dados do board
     const fetchData = async () => {
       try {
         const columnsRes = await monday.api(main, {
           apiVersion: "2023-10",
         });
         setBoardData(columnsRes.data.boards[0]);
-        console.log("comunsRes", columnsRes);
+        setColumns(
+          columnsRes.data.boards[0].columns
+            .filter(
+              (item: { id: string; title: string }) => item.id !== "location" // if location column is included, it returns status 500 in filter queries
+            )
+            .map((item: { id: string }) => {
+              return item.id;
+            })
+        );
+        console.log("columnsRes", columnsRes);
       } catch (error: any) {
-        //setError(error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, []);
 
+  useEffect(() => {
+    console.log("searchTerm", searchTerm);
+    console.log("columns", columns);
+    const handleSearch = async () => {
+      if (searchTerm) {
+        // dynamic rules for searching the term in every column (except location)
+        const rules = columns
+          .map(
+            (column) =>
+              `{ column_id: "${column}", compare_value: "${searchTerm}" }`
+          )
+          .join(", ");
+        console.log("rules", rules);
+
+        const searchQuery = `query {
+          boards(ids: 6756792083) {
+            items_page(query_params: {rules: [${rules}], operator: or}) {
+              items {
+                id 
+                name
+                column_values {
+                  id
+                  type
+                  value
+                }
+              }
+            }
+          }    
+        }`;
+        console.log("searchQuery", searchQuery);
+
+        try {
+          const searchRes = await monday.api(searchQuery, {
+            apiVersion: "2023-10",
+          });
+          console.log("searchRes", searchRes);
+          console.log("searchRes", searchRes.data.boards[0].items_page.items);
+
+          setResultData(searchRes.data.boards[0].items_page.items);
+        } catch (error: any) {
+          console.error("Error searching data:", error);
+        }
+      } else {
+        setResultData([]); // Limpa os resultados da pesquisa se o campo de pesquisa estiver vazio
+      }
+    };
+
+    handleSearch();
+  }, [searchTerm]);
+
   if (!boardData) {
     return (
-      <Flex align="Center" justify="Center" className="loading-box">
+      <Flex align="center" justify="center" className="loading-box">
         <Loader color="var(--primary-color)" size={40} />
       </Flex>
     );
   }
 
   return (
-    <Box backgroundColor="Box-module_bgPrimaryBackgroundColor">
-      <Search placeholder="Placeholder text here" />
-      {/*  <h1>Board Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Item Name</th>
-            {boardData.columns.map((column) => (
-              <th key={column.id}>{column.title}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {boardData.items_page.items.map((item) => (
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              {item.column_values.map((value) => (
-                <td>{value.value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
-    </Box>
+    <div className="main">
+      <div className="search-box">
+        <Search
+          placeholder="Busque informações do país"
+          onChange={(e) => setSearchTerm(e)}
+        />
+      </div>
+      <Box textColor="Box-module_textPrimaryTextColor" className="result-box">
+        {resultData.length > 0 ? (
+          resultData.map((item: any) => (
+            <Box key={item.id}>
+              <h3>{item.name}</h3>
+              {/* {item.column_values.map((column: any) => (
+                <Text type={Text.types.TEXT1} key={column.id}>
+                  {column.type}: {column.value ? column.value : "N/A"}
+                </Text>
+              ))} */}
+            </Box>
+          ))
+        ) : (
+          <p>No results</p>
+        )}
+      </Box>
+    </div>
   );
 }
 
